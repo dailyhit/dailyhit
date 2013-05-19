e42a68bf4b77753220fdfc988fc3099b090bc031
crazy0man <angelo.yin@gmail.com>
2013-05-19 19:54:36 +0800

    update idea

--- idea/dailyhit-idea.mm
+++ idea/dailyhit-idea.mm
@@ -1,65 +1,247 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1368748468197" ID="ID_1023587719" MODIFIED="1368749653805" TEXT="dailyhit idea">
-<node CREATED="1368748520323" ID="ID_731817671" MODIFIED="1368748525210" POSITION="right" TEXT="Target">
-<node CREATED="1368748526700" ID="ID_595642000" MODIFIED="1368748548252" TEXT="Help developer or persons to pragmatic working"/>
-<node CREATED="1368748551097" ID="ID_667432182" MODIFIED="1368748559682" TEXT="Do daily task management"/>
-<node CREATED="1368748560397" ID="ID_474696494" MODIFIED="1368748565949" TEXT="time management"/>
-<node CREATED="1368748567760" ID="ID_1742277866" MODIFIED="1368748571624" TEXT="idea management"/>
-<node CREATED="1368748573283" ID="ID_1209202524" MODIFIED="1368748581533" TEXT="Work analysis"/>
-<node CREATED="1368748584265" ID="ID_1772718742" MODIFIED="1368748592139" TEXT="improve himself"/>
+<node COLOR="#000000" CREATED="1368748468197" ID="ID_733273942" LINK="../../working/freemind_root/Project-Idea.mm" MODIFIED="1368926150905" TEXT="dailyhit idea">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="20"/>
+<node COLOR="#0033ff" CREATED="1368748520323" ID="ID_1884211553" MODIFIED="1368926148094" POSITION="right" TEXT="Target">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#ffff00" COLOR="#00b439" CREATED="1368748526700" ID="ID_327725770" MODIFIED="1368926148095" TEXT="Help developer or persons to pragmatic working">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
 </node>
-<node CREATED="1368748602002" ID="ID_1897051919" MODIFIED="1368748608820" POSITION="left" TEXT="Develop Mode">
-<node CREATED="1368748609822" ID="ID_1948724242" MODIFIED="1368748612533" TEXT="Open Source"/>
-<node CREATED="1368748613176" ID="ID_64489586" MODIFIED="1368748622594" TEXT="Free for unbusiness using"/>
-<node CREATED="1368748623113" ID="ID_1642852582" MODIFIED="1368748629449" TEXT="Charge for business using"/>
+<node BACKGROUND_COLOR="#ffff00" COLOR="#00b439" CREATED="1368748560397" ID="ID_696781087" MODIFIED="1368926148095" TEXT="time management">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
 </node>
-<node CREATED="1368748637452" ID="ID_126590885" MODIFIED="1368749665210" POSITION="right" TEXT="Features">
-<node CREATED="1368748701029" ID="ID_596105380" MODIFIED="1368748711166" TEXT="Access anywhere and anytime"/>
-<node CREATED="1368748713754" ID="ID_19581620" MODIFIED="1368748722756" TEXT="apply pragmatic"/>
-<node CREATED="1368748968427" ID="ID_1642972988" MODIFIED="1368748983046" TEXT="apply &quot;The Effective Executive&quot;"/>
-<node CREATED="1368748983915" ID="ID_435834458" MODIFIED="1368748995902" TEXT="apply PMI"/>
-<node CREATED="1368748996441" ID="ID_1874134188" MODIFIED="1368749015376" TEXT="apply other effective methodology"/>
-<node CREATED="1368749142764" ID="ID_1862731197" MODIFIED="1368749163843" TEXT="portalbe: from job to job, from windows to linux, mac"/>
-<node CREATED="1368749164863" ID="ID_1271507814" MODIFIED="1368749190352" TEXT="historical: all items are under version control"/>
-<node CREATED="1368749217025" ID="ID_88791848" MODIFIED="1368749245663" TEXT="timeline:supply timeline for someitem, you can conclude and learn from your historyfrom "/>
-<node CREATED="1368749248713" ID="ID_1139839756" MODIFIED="1368749282615" TEXT="analysiable: supply some time and efficiency analysis model"/>
-<node CREATED="1368749283411" ID="ID_750573328" MODIFIED="1368749318874" TEXT="improvable: along with time and using, suggestion to improve the working habit"/>
-<node CREATED="1368749320357" ID="ID_997768552" MODIFIED="1368749363848" TEXT="customizable: all things can be customized by peronal with his habit, because efficiency is in context"/>
-<node CREATED="1368749366829" ID="ID_441933633" MODIFIED="1368749386163" TEXT="Personal: all things keep in security"/>
-<node CREATED="1368749387111" ID="ID_587740733" MODIFIED="1368749398850" TEXT="Free: free for personal using"/>
-<node CREATED="1368749401278" ID="ID_1695110445" MODIFIED="1368749412785" TEXT="Effective:?"/>
-<node CREATED="1368749413300" ID="ID_1573751511" MODIFIED="1368749447931" TEXT="Flexible: easy to work with other existing tools"/>
-<node CREATED="1368749450270" ID="ID_1826618082" MODIFIED="1368749466404" TEXT="Mentor: from novice to expert"/>
-<node CREATED="1368749615322" ID="ID_1034641608" MODIFIED="1368749634834" TEXT="Share: easily share ideas and experiences worldwide"/>
-<node CREATED="1368749674496" ID="ID_1091288103" MODIFIED="1368749714842" TEXT="unlimit: keep your life in history, record all your experience of all your life"/>
+<node BACKGROUND_COLOR="#ffff00" COLOR="#00b439" CREATED="1368748567760" ID="ID_966816443" MODIFIED="1368926148096" TEXT="idea management">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
 </node>
-<node CREATED="1368748680073" ID="ID_157339739" MODIFIED="1368748691093" POSITION="left" TEXT="Others">
-<node CREATED="1368748643150" ID="ID_514382791" MODIFIED="1368748671915" TEXT="having pluging with many existing tools, such as outlook"/>
+<node COLOR="#00b439" CREATED="1368926035591" ID="ID_286749813" MODIFIED="1368926148097" TEXT="task management">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1368926050156" ID="ID_487881492" MODIFIED="1368926148098" TEXT="task plan">
+<font NAME="SansSerif" SIZE="14"/>
 </node>
-<node CREATED="1368749025040" ID="ID_677495029" MODIFIED="1368749026981" POSITION="right" TEXT="Users">
-<node CREATED="1368749029530" ID="ID_807113410" MODIFIED="1368749039332" TEXT="Developer, IT professonal"/>
-<node CREATED="1368749039887" ID="ID_574601862" MODIFIED="1368749050650" TEXT="people in other domain"/>
+<node COLOR="#990000" CREATED="1368926045223" ID="ID_14109661" MODIFIED="1368926148098" TEXT="todo list">
+<font NAME="SansSerif" SIZE="14"/>
 </node>
-<node CREATED="1368749067421" ID="ID_666149506" MODIFIED="1368749070708" POSITION="left" TEXT="functional">
-<node CREATED="1368749072079" ID="ID_1183205653" MODIFIED="1368749077455" TEXT="supply version control"/>
-<node CREATED="1368749081237" ID="ID_632875609" MODIFIED="1368749129525" TEXT="permanent storage"/>
+<node COLOR="#990000" CREATED="1368748551097" ID="ID_410957407" MODIFIED="1368926077035" TEXT="Do daily task management">
+<font NAME="SansSerif" SIZE="14"/>
 </node>
-<node CREATED="1368749476724" ID="ID_1683055609" MODIFIED="1368749482477" POSITION="right" TEXT="Technique">
-<node CREATED="1368749486585" ID="ID_1249487842" MODIFIED="1368749505600" TEXT="Should use some crossplatform, such mono"/>
-<node CREATED="1368749506667" ID="ID_1894458541" MODIFIED="1368749520567" TEXT="Platform to consider">
-<node CREATED="1368749522131" ID="ID_652627686" MODIFIED="1368749523161" TEXT="PC">
-<node CREATED="1368749546800" ID="ID_1799355479" MODIFIED="1368749548254" TEXT="Linux"/>
-<node CREATED="1368749548866" ID="ID_1021976979" MODIFIED="1368749551983" TEXT="MAC"/>
-<node CREATED="1368749552851" ID="ID_131670205" MODIFIED="1368749554505" TEXT="Windows"/>
 </node>
-<node CREATED="1368749523846" ID="ID_832750672" MODIFIED="1368749526306" TEXT="Mobile"/>
-<node CREATED="1368749527382" ID="ID_1189982464" MODIFIED="1368749545076" TEXT="other"/>
+<node COLOR="#00b439" CREATED="1368748573283" ID="ID_1994023770" MODIFIED="1368926148099" TEXT="Work analysis">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
 </node>
-<node CREATED="1368749565914" ID="ID_810107813" MODIFIED="1368749573391" TEXT="Model">
-<node CREATED="1368749576143" ID="ID_1677426168" MODIFIED="1368749585497" TEXT="Desktop Client"/>
-<node CREATED="1368749586069" ID="ID_666184901" MODIFIED="1368749588354" TEXT="Web"/>
-<node CREATED="1368749589517" ID="ID_449569000" MODIFIED="1368749604818" TEXT="Cloud sync and access"/>
+<node COLOR="#00b439" CREATED="1368748584265" ID="ID_246940934" MODIFIED="1368926148100" TEXT="improve himself">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ffff00" COLOR="#00b439" CREATED="1368925433635" ID="ID_345009535" MODIFIED="1368926148101" TEXT="personalized work environment">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ffff00" COLOR="#00b439" CREATED="1368925448451" ID="ID_404974475" MODIFIED="1368926148102" TEXT="portable work environment">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ffff00" COLOR="#00b439" CREATED="1368925471581" ID="ID_1839736942" MODIFIED="1368926148103" TEXT="syncronized work environment">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ffff00" COLOR="#00b439" CREATED="1368925508875" ID="ID_1289183880" MODIFIED="1368926148104" TEXT="free personal use">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ffff00" COLOR="#00b439" CREATED="1368925525678" ID="ID_863230698" MODIFIED="1368926148105" TEXT="apply export working pattern">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ffff00" COLOR="#0033ff" CREATED="1368748602002" ID="ID_816369560" MODIFIED="1368926148106" POSITION="right" TEXT="business model">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1368748613176" ID="ID_1734988537" MODIFIED="1368926148107" TEXT="Free for unbusiness using">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1368748623113" ID="ID_643409429" MODIFIED="1368926148108" TEXT="Charge for business using">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1368926115869" ID="ID_1710459868" MODIFIED="1368926148109" POSITION="right" TEXT="develop model">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#0033ff" CREATED="1368926127771" ID="ID_777843824" MODIFIED="1368926201957" TEXT="open source">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1368926131476" ID="ID_1535529972" MODIFIED="1368926148110" TEXT="TDD">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node CREATED="1368926133546" ID="ID_1385101750" MODIFIED="1368926184601" TEXT="agile">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1368748609822" ID="ID_209906488" MODIFIED="1368926148111" TEXT="Open Source">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1368748637452" ID="ID_520147090" MODIFIED="1368926148111" POSITION="right" TEXT="Features">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1368748701029" ID="ID_1094533686" MODIFIED="1368926148112" TEXT="Access anywhere and anytime">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1368748713754" ID="ID_149112997" MODIFIED="1368926148112" TEXT="apply pragmatic">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1368748968427" ID="ID_1723190" MODIFIED="1368926148115" TEXT="apply &quot;The Effective Executive&quot;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1368748983915" ID="ID_877501246" MODIFIED="1368926148116" TEXT="apply PMI">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1368748996441" ID="ID_1498585495" MODIFIED="1368926148116" TEXT="apply other effective methodology">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1368749142764" ID="ID_743031387" MODIFIED="1368926148117" TEXT="portalbe: from job to job, from windows to linux, mac">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1368749164863" ID="ID_2760138" MODIFIED="1368926148117" TEXT="historical: all items are under version control">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1368749217025" ID="ID_42425818" MODIFIED="1368926148132" TEXT="timeline:supply timeline for someitem, you can conclude and learn from your historyfrom ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1368749248713" ID="ID_1538527672" MODIFIED="1368926148135" TEXT="analysiable: supply some time and efficiency analysis model">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1368749283411" ID="ID_1237846839" MODIFIED="1368926148140" TEXT="improvable: along with time and using, suggestion to improve the working habit">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1368749320357" ID="ID_1621917544" MODIFIED="1368926148157" TEXT="customizable: all things can be customized by peronal with his habit, because efficiency is in context">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1368749366829" ID="ID_65904962" MODIFIED="1368926148161" TEXT="Personal: all things keep in security">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1368749387111" ID="ID_209436031" MODIFIED="1368926148161" TEXT="Free: free for personal using">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1368749401278" ID="ID_1909306894" MODIFIED="1368926148162" TEXT="Effective:?">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1368749413300" ID="ID_1173501867" MODIFIED="1368926148162" TEXT="Flexible: easy to work with other existing tools">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1368749450270" ID="ID_229787675" MODIFIED="1368926148162" TEXT="Mentor: from novice to expert">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1368749615322" ID="ID_738801078" MODIFIED="1368926148163" TEXT="Share: easily share ideas and experiences worldwide">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1368749674496" ID="ID_1153639334" MODIFIED="1368926148163" TEXT="unlimit: keep your life in history, record all your experience of all your life">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1368748680073" ID="ID_132720413" MODIFIED="1368926148164" POSITION="right" TEXT="Others">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1368748643150" ID="ID_1272161261" MODIFIED="1368926148164" TEXT="having pluging with many existing tools, such as outlook">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1368749025040" ID="ID_737706045" MODIFIED="1368926148165" POSITION="right" TEXT="Users">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1368749029530" ID="ID_1298245773" MODIFIED="1368926148165" TEXT="Developer, IT professonal">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1368749039887" ID="ID_1267609400" MODIFIED="1368926148165" TEXT="people in other domain">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ffff00" COLOR="#00b439" CREATED="1368925202496" ID="ID_175879230" MODIFIED="1368926148166" TEXT="people who care about his efficiency">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1368926091939" ID="ID_76676870" MODIFIED="1368926148166" TEXT="managers">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1368749067421" ID="ID_966432093" MODIFIED="1368926148167" POSITION="right" TEXT="functional">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1368749072079" ID="ID_625286264" MODIFIED="1368926148167" TEXT="supply version control">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1368749081237" ID="ID_621398384" MODIFIED="1368926148168" TEXT="permanent storage">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1368749476724" ID="ID_111657081" MODIFIED="1368926148168" POSITION="right" TEXT="Technique">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1368749486585" ID="ID_971872213" MODIFIED="1368926148168" TEXT="Should use some crossplatform, such mono">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1368749506667" ID="ID_355250819" MODIFIED="1368926148169" TEXT="Platform to consider">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1368749522131" ID="ID_779052879" MODIFIED="1368926148169" TEXT="PC">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1368749546800" ID="ID_838049534" MODIFIED="1368925015414" TEXT="Linux"/>
+<node COLOR="#111111" CREATED="1368749548866" ID="ID_1771848578" MODIFIED="1368925015414" TEXT="MAC"/>
+<node COLOR="#111111" CREATED="1368749552851" ID="ID_585225110" MODIFIED="1368925015414" TEXT="Windows"/>
+</node>
+<node COLOR="#990000" CREATED="1368749523846" ID="ID_697596464" MODIFIED="1368926148170" TEXT="Mobile">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1368749527382" ID="ID_932256595" MODIFIED="1368926148170" TEXT="other">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1368749565914" ID="ID_1379753130" MODIFIED="1368926148170" TEXT="Model">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1368749576143" ID="ID_1064238308" MODIFIED="1368926148170" TEXT="Desktop Client">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1368749586069" ID="ID_1429077143" MODIFIED="1368926148171" TEXT="Web">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1368749589517" ID="ID_1849314680" MODIFIED="1368926148171" TEXT="Cloud sync and access">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
 </node>
 </node>
 </node>
